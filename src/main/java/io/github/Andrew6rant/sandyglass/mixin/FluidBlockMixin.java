190d9a1764661a36423375cf5b2ea9b1e5ca5154
Andrew Grant <andrew.grant@mail.rit.edu>
2023-07-11 23:38:26 -0400

    implement lava flow logic
    flowing lava will turn sand into sandy glass, and red sand into red sandy glass

--- src/main/java/io/github/Andrew6rant/sandyglass/mixin/FluidBlockMixin.java
+++ src/main/java/io/github/Andrew6rant/sandyglass/mixin/FluidBlockMixin.java
@@ -25,12 +25,16 @@ public abstract class FluidBlockMixin {
 
     @Inject(method = "receiveNeighborFluids(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z",
     at = @At(value = "INVOKE", target = "Lnet/minecraft/fluid/FluidState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private void sandyglass$receiveNeighborFluids(World world, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir, boolean bl, UnmodifiableIterator var5, Direction direction, BlockPos blockPos) {
-        System.out.println("inject");
-        if (world.getBlockState(blockPos).isOf(Blocks.SAND)) {
-            System.out.println("sand found");
-            world.setBlockState(pos, SandyGlass.SANDY_GLASS.getDefaultState());
-            this.playExtinguishSound(world, pos);
+    private void sandyglass$receiveNeighborFluids(World world, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir, boolean bl, UnmodifiableIterator var5, Direction direction) {
+        BlockPos directionPos = pos.offset(direction);
+        if (world.getBlockState(directionPos).isOf(Blocks.SAND)) {
+            world.setBlockState(directionPos, SandyGlass.SANDY_GLASS.getDefaultState());
+            this.playExtinguishSound(world, directionPos);
+            cir.setReturnValue(false);
+        }
+        else if (world.getBlockState(directionPos).isOf(Blocks.RED_SAND)) {
+            world.setBlockState(directionPos, SandyGlass.RED_SANDY_GLASS.getDefaultState());
+            this.playExtinguishSound(world, directionPos);
             cir.setReturnValue(false);
         }
     }
